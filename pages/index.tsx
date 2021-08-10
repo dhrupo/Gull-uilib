af3d3cceee4584afaf6b36aec895eabddbdd9313
dhrupo <dhrupo@gmail.com>
2021-08-10 12:42:19 +0600

    deploy issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Box, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import Dashboard from "../src/components/Dashboard";
 
 export default function Index() {
